e6fb9014668ecc2d1fb03ba3a2c8548603bd58d9
Dewey <hammersdrichard@gmail.com>
2020-12-22 16:06:18 -0800

    commit tree d3d03cb2455e5787bfd083add4bdacc6631d637a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+    attr_accessor :roster 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster.key?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        if @roster.key?(grade)
+            @roster[grade]
+        end
+    end
+
+    def sort
+        @roster.map do |key, value|
+            value.sort!
+        end
+        @roster
+    end
+end
